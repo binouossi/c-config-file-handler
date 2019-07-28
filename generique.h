85122c15cfd37008140a3363e055807b43f9649e
binouossi <tacolodjounini@gmail.com>
2019-07-29 00:04:16 +0100

    initialisation

---
+++ generique.h
@@ -0,0 +1,38 @@
+
+#ifndef GENERIQUE_H
+#define GENERIQUE_H
+
+
+
+#include<fstream>
+
+#include <iostream>
+
+#include<sstream>
+
+#include<string.h>
+
+
+
+// this struct content the values that will be read from configuration file
+// it will be customized deppending on the demmands of parameters that
+// will be read from config file
+// Note that the stat here is to determine if  the operation has been done.
+// it act as operation control variable
+
+struct con{
+    char* addr;
+    int port;
+    bool stat=false;
+};
+
+// this function return the readed data struct
+// It take the config file absolut path as parameter
+con nt_config(std::string path);
+
+auto get_value(con&,bool&,std::istringstream&);
+
+
+
+
+#endif // GENERIQUE_H
